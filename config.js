b17f71bc7d7211200a5192115d1028c9953759ea
llrznhy <97338618+llrznhy@users.noreply.github.com>
2024-08-03 22:14:06 +1000

    Update config.js

--- config.js
+++ config.js
@@ -30,13 +30,15 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "点我点我",
+        play: "来点音乐",
+        bannar_coming: "高点装饰",
+        balloons_flying: "好像还少点",
+        cake_fadein: "蛋糕呢",
+        light_candle: "蜡烛呢",
+        wish_message: "生日快乐~",
         story: "A MESSAGE FOR YOU",
-    }
+    },
+    // 结束语
+    loveText: '爱你的老腊肉'
 };
