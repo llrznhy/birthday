f404b3d159580cde4ad919f1b293b1d0f9144bc7
llrznhy <97338618+llrznhy@users.noreply.github.com>
2024-09-04 22:45:16 +1000

    Delete test/Server directory

--- test/Server/SCWeb/src/main/java/com/example/SC/utils/hash256Utils.java
+++
@@ -1,9 +0,0 @@
-package com.example.SC.utils;
-
-import org.apache.commons.codec.digest.DigestUtils;
-
-public class hash256Utils {
-    public static String encodeSHA256(String data){
-        return DigestUtils.sha256Hex(data);
-    }
-}
